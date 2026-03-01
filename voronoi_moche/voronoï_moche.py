9045811c7bcd536d7ebe41bd2f3db22b4d79eae5
Lorenzo JACCAUD--GODEFROY <lorenzojg@hotmail.fr>
2026-03-01 01:44:53 +0100

    Refactorisation definitive.

--- voronoi_moche/voronoï_moche.py
+++ voronoi_moche/voronoï_moche.py
@@ -4,8 +4,6 @@ from point import Point
 from json import load
 from math import sqrt
 
-#coucou les loulous ça marche !!!
-
 def json_loader(chemin:str="liste_de_sites.json"):
 
     with open(chemin, "r", encoding="utf-8") as fp:
@@ -15,7 +13,7 @@ def json_loader(chemin:str="liste_de_sites.json"):
 
 def dictionnaire_germes(germes:list[Point], pixel:Point):
     dico_distances = {}
-    for germe in germes: # À VOIR
+    for germe in germes:
         dico_distances[germe]=germe.distance_to(pixel)
     return dico_distances
 
@@ -43,39 +41,27 @@ def coloriage(height, width, espacement, germes):
         dico_couleur[germe]=c
         c+=1
 
-    for i in range(0,height,espacement):
-        for j in range(0,width,espacement):
-            voronoi_diagram[i][j] = dico_couleur[germe_le_plus_proche(germes, pixel = Point(j, i))]
+    for i in range(0,width,espacement):
+        for j in range(0,height,espacement):
+            voronoi_diagram[j][i] = dico_couleur[germe_le_plus_proche(germes, pixel = Point(i, j))]
         
 
     plt.imshow(voronoi_diagram,origin='lower')
-
-            
-
-
-def voronoi_moche(fig,liste_points:Point):
-    pass
 
 if __name__ == "__main__":
     
     fig = plt.figure()
 
+    #donnees_points_brutes = json_loader()
+    #points = Points(donnees_points_brutes)
+
     germes_exemple = [Point(2, 3),Point(98,34),Point(12,45),Point(34,56),Point(78,90),Point(23,67),Point(45,23),Point(56,78),Point(67,12),Point(89,34)]
-    # exemple : on trace un grillage de 4×2 avec espacement 0.5
-    #def_grillage(fig, width=10, height=10, spacing=1)
+
     afficher_germe(germes_exemple)
     coloriage(100,100,1,germes_exemple)
 
     plt.show()  # affiche le grillage
-    #donnees_points_brutes = json_loader()
-    #points = Points(donnees_points_brutes)
-    #
-    #afficher_point(fig,points)
-    #
-    #quadrillage_extremum = points.get_extrumum_points_of_cadrillage()
-    #grillage(fig,quadrillage_extremum)
-    #
-    #voronoi_moche(fig,points)
+
     
     plt.savefig("matplotlib_grid_01.png", bbox_inches='tight')
 
