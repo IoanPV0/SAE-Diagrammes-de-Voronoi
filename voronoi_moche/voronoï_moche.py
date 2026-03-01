2cbe723626d166d86a9af60f98a5fac2f691befc
RaziHadjAmor <razi.hadjamor@gmail.com>
2026-03-01 14:06:24 +0100

    Suppression des fichiers inutilisés et refactorisation de la fonction pour récupérer les germes depuis un fichier .txt

--- voronoi_moche/voronoï_moche.py
+++ voronoi_moche/voronoï_moche.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from point import Point
-from json import load
-from math import sqrt
 
-def json_loader(chemin:str="liste_de_sites.json"):
 
-    with open(chemin, "r", encoding="utf-8") as fp:
-        données = load(fp)
+def txt_reader_et_generateur_point(chemin:str="germes.txt"):
 
-    return données
+    with open(chemin, 'r') as f:
+        lignes = f.readlines()
+
+    liste_de_germes = []
+    x,y = 0,0
+    for ligne in lignes:
+        parties = ligne.split(",")
+        x = float(parties[0])
+        y = float(parties[1])
+        point = Point(x,y)
+        liste_de_germes.append(point)
+    return liste_de_germes
 
 def dictionnaire_germes(germes:list[Point], pixel:Point):
     dico_distances = {}
@@ -29,7 +36,7 @@ def germe_le_plus_proche(germes:list[Point], pixel:Point):
 
 def afficher_germe(germes:list[Point]):
     for germe in germes:
-        plt.scatter(germe.x, germe.y, color='red', marker='x')
+        plt.scatter(germe.x, germe.y, color='red', marker='o')
 
 def coloriage(height, width, espacement, germes):
 
@@ -45,24 +52,17 @@ def coloriage(height, width, espacement, germes):
         for j in range(0,height,espacement):
             voronoi_diagram[j][i] = dico_couleur[germe_le_plus_proche(germes, pixel = Point(i, j))]
         
-
     plt.imshow(voronoi_diagram,origin='lower')
 
 if __name__ == "__main__":
     
     fig = plt.figure()
 
-    #donnees_points_brutes = json_loader()
-    #points = Points(donnees_points_brutes)
+    germes_fichier = txt_reader_et_generateur_point()
 
-    germes_exemple = [Point(2, 3),Point(98,34),Point(12,45),Point(34,56),Point(78,90),Point(23,67),Point(45,23),Point(56,78),Point(67,12),Point(89,34)]
-
-    afficher_germe(germes_exemple)
-    coloriage(100,100,1,germes_exemple)
+    afficher_germe(germes_fichier)
+    coloriage(100,100,1,germes_fichier)
 
     plt.show()  # affiche le grillage
-
-    
     plt.savefig("matplotlib_grid_01.png", bbox_inches='tight')
-
     plt.close()
